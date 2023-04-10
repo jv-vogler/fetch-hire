7c827af18b4b7fdb6ea1fc1e8295dffab1697284
jv-vogler <jvsvogler@gmail.com>
2023-04-10 19:13:24 -0300

    refactor: :label: extract subtypes from GithubData for more flexibility

--- src/types/GithubData.ts
+++ src/types/GithubData.ts
@@ -1,61 +1,5 @@
 export interface GithubData {
-  user: {
-    avatarUrl: string
-    bio: string | null
-    company: string | null
-    createdAt: string
-    email: string | null
-    isHireable: boolean
-    location: string | null
-    login: string
-    name: string | null
-    pronouns: string | null
-    url: string
-    followers: {
-      totalCount: number
-    }
-    websiteUrl: string | null
-    socialAccounts: {
-      nodes: {
-        url: string
-        provider: string
-      }[]
-    }
-    contributionsCollection: {
-      totalRepositoryContributions: number
-      totalRepositoriesWithContributedCommits: number
-      startedAt: string
-      contributionCalendar: {
-        totalContributions: number
-      }
-    }
-    repositories: {
-      edges: {
-        node: {
-          url: string
-          name: string
-          updatedAt: string
-          isFork: boolean
-          forkCount: number
-          description: string | null
-          createdAt: string
-          isEmpty: boolean
-          languages: {
-            edges: {
-              node: {
-                name: string
-                color: string
-              }
-              size: number
-            }[]
-          }
-          object: {
-            text: string | null
-          }
-        }
-      }[]
-    }
-  }
+  user: User
   rateLimit: {
     cost: number
     nodeCount: number
@@ -64,4 +8,68 @@ export interface GithubData {
     resetAt: string
     used: number
   }
+}
+
+export type User = {
+  avatarUrl: string
+  bio: string | null
+  company: string | null
+  createdAt: string
+  email: string | null
+  isHireable: boolean
+  location: string | null
+  login: string
+  name: string | null
+  pronouns: string | null
+  url: string
+  followers: {
+    totalCount: number
+  }
+  websiteUrl: string | null
+  socialAccounts: {
+    nodes: {
+      url: string
+      provider: string
+    }[]
+  }
+  contributionsCollection: {
+    totalRepositoryContributions: number
+    totalRepositoriesWithContributedCommits: number
+    startedAt: string
+    contributionCalendar: {
+      totalContributions: number
+    }
+  }
+  repositories: {
+    edges: Repository[]
+  }
+}
+
+export type Repository = {
+  node: {
+    url: string
+    name: string
+    updatedAt: string
+    isFork: boolean
+    forkCount: number
+    description: string | null
+    createdAt: string
+    isEmpty: boolean
+    languages: Languages
+    object: {
+      text: string | null
+    }
+  }
+}
+
+export type Language = {
+  node: {
+    name: string
+    color: string
+  }
+  size: number
+}
+
+export type Languages = {
+  edges: Language[]
 }
