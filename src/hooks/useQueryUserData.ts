8b84277302c997cdefe46f15ce709a75f741cd89
jv-vogler <jvsvogler@gmail.com>
2023-04-11 19:48:24 -0300

    refactor: :wrench: update react-query to retry fetching 2 times

--- src/hooks/useQueryUserData.ts
+++ src/hooks/useQueryUserData.ts
@@ -2,17 +2,17 @@ import { useQuery } from '@tanstack/react-query'
 
 import { fetchUserData } from '@/services/github/githubApi'
 
-const CACHE_DURATIION_MINS = 10
+const CACHE_DURATIION_MINS = 30
 
 export const useQueryUserData = (login: string) => {
   return useQuery({
     queryKey: ['user-data', login],
     queryFn: () => fetchUserData(login),
     enabled: !!login,
-    retry: false,
+    retry: 2,
     retryOnMount: false,
     refetchOnWindowFocus: false,
-    cacheTime: CACHE_DURATIION_MINS * 60 * 1000,
     staleTime: Infinity,
+    cacheTime: CACHE_DURATIION_MINS * 60 * 1000,
   })
 }
