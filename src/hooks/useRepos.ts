6f0ca4028fdc23a9c7a43717583a1d23d046a0c5
jv-vogler <jvsvogler@gmail.com>
2023-04-12 13:25:44 -0300

    refactor: :recycle: make use of a single useMemo inside useRepos hook

--- src/hooks/useRepos.ts
+++ src/hooks/useRepos.ts
@@ -8,66 +8,62 @@ type Props = Repository[] | undefined
 type LangData = { name: string; color: string; size: number }
 
 export const useRepos = (repos: Props) => {
-  const languages = useCalculateLanguages(repos)
-  const technologies = useCaculateTechnologies(repos)
+  const data = useMemo(() => {
+    const languages = calculateLanguages(repos)
+    const technologies = caculateTechnologies(repos)
 
-  return { languages, technologies }
-}
-
-const useCaculateTechnologies = (repos: Props) => {
-  const technologies = useMemo(() => {
-    if (!repos) return
-
-    const initialDependenciesList = {
-      packageJsonList: [] as string[],
-      requirementsList: [] as string[],
-      gemfileList: [] as string[],
-      composerList: [] as string[],
-    }
-
-    const dependenciesLists = repos.reduce((acc, repo) => {
-      const { packageJson, requirements, gemfile, composer } = repo.node
-      packageJson && packageJson.text && acc.packageJsonList.push(packageJson.text)
-      requirements && requirements.text && acc.requirementsList.push(requirements.text)
-      gemfile && gemfile.text && acc.gemfileList.push(gemfile.text)
-      composer && composer.text && acc.composerList.push(composer.text)
-
-      return acc
-    }, initialDependenciesList)
-
-    const technologiesUsed = {
-      ...parsePackageJsonList(dependenciesLists.packageJsonList),
-      //TODO add other parse methods
-    }
-
-    return technologiesUsed
+    return { languages, technologies }
   }, [repos])
 
-  return technologies
+  return data
 }
 
-const useCalculateLanguages = (repos: Props) => {
-  const languages = useMemo(() => {
-    if (!repos) return
+const caculateTechnologies = (repos: Props) => {
+  if (!repos) return
 
-    const mergedLanguages = repos
-      .flatMap(repo => repo.node.languages.edges)
-      .reduce<Record<string, LangData>>((acc, lang) => accumulateLangData(acc, lang), {})
+  const initialDependenciesList = {
+    packageJsonList: [] as string[],
+    requirementsList: [] as string[],
+    gemfileList: [] as string[],
+    composerList: [] as string[],
+  }
 
-    const languagesArray = Object.values(mergedLanguages).sort((a, b) => b.size - a.size)
+  const dependenciesLists = repos.reduce((acc, repo) => {
+    const { packageJson, requirements, gemfile, composer } = repo.node
+    packageJson && packageJson.text && acc.packageJsonList.push(packageJson.text)
+    requirements && requirements.text && acc.requirementsList.push(requirements.text)
+    gemfile && gemfile.text && acc.gemfileList.push(gemfile.text)
+    composer && composer.text && acc.composerList.push(composer.text)
 
-    const totalSize = languagesArray.reduce((acc, lang) => acc + lang.size, 0)
+    return acc
+  }, initialDependenciesList)
 
-    const languagesWithPercentage = languagesArray.map(lang => ({
-      name: lang.name,
-      color: lang.color,
-      percentage: ((lang.size / totalSize) * 100).toFixed(2),
-    }))
+  const technologiesUsed = {
+    ...parsePackageJsonList(dependenciesLists.packageJsonList),
+    //TODO add other parse methods
+  }
 
-    return languagesWithPercentage
-  }, [repos])
+  return technologiesUsed
+}
 
-  return languages
+const calculateLanguages = (repos: Props) => {
+  if (!repos) return
+
+  const mergedLanguages = repos
+    .flatMap(repo => repo.node.languages.edges)
+    .reduce<Record<string, LangData>>((acc, lang) => accumulateLangData(acc, lang), {})
+
+  const languagesArray = Object.values(mergedLanguages).sort((a, b) => b.size - a.size)
+
+  const totalSize = languagesArray.reduce((acc, lang) => acc + lang.size, 0)
+
+  const languagesWithPercentage = languagesArray.map(lang => ({
+    name: lang.name,
+    color: lang.color,
+    percentage: ((lang.size / totalSize) * 100).toFixed(2),
+  }))
+
+  return languagesWithPercentage
 }
 
 const accumulateLangData = (acc: Record<string, LangData>, lang: Language) => {
