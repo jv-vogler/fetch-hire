b44a7a4dd8216809677a70ed8e905bc92bbb7dd7
jv-vogler <jvsvogler@gmail.com>
2023-04-17 14:50:14 -0300

    refactor: :recycle: utilize a custom 'status' instead of react query's

--- src/hooks/useQueryUserData.ts
+++ src/hooks/useQueryUserData.ts
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { persistQueryClient } from '@tanstack/react-query-persist-client'
 
@@ -8,6 +10,7 @@ const CACHE_DURATION_MINS = 60
 
 export const useQueryUserData = (login: string) => {
   const queryClient = useQueryClient()
+  const [status, setStatus] = useState<'loading' | 'success' | 'error'>('success')
 
   if (typeof window !== 'undefined') {
     const idbPersister = createIDBPersister('git-hire-cache')
@@ -19,16 +22,36 @@ export const useQueryUserData = (login: string) => {
     })
   }
 
-  return useQuery({
+  const {
+    data,
+    error,
+    status: rqStatus,
+  } = useQuery({
     queryKey: ['user-data', login.toLowerCase()],
     queryFn: () => fetchUserData(login),
     enabled: !!login,
     retry: 3,
-    retryDelay: attemptIndex => Math.min(3000 * 2 ** attemptIndex, 30000),
+    retryDelay: attemptIndex => Math.min(6000 * 2 ** attemptIndex, 30000),
     retryOnMount: true,
     refetchOnMount: true,
     refetchOnWindowFocus: false,
     cacheTime: CACHE_DURATION_MINS * 60 * 1000,
     staleTime: Infinity,
   })
+
+  useEffect(() => {
+    switch (rqStatus) {
+      case 'success':
+        data?.user ? setStatus('success') : setStatus('error')
+        break
+      case 'loading':
+        login ? setStatus('loading') : setStatus('success')
+        break
+      case 'error':
+        setStatus('error')
+        break
+    }
+  }, [login, data, rqStatus])
+
+  return { data, error, status }
 }
