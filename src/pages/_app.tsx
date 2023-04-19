1e21c0b7f8624036f41d75078be9d5d1493eb792
jv-vogler <jvsvogler@gmail.com>
2023-04-19 15:18:18 -0300

    feat: :art: add Poppins font globally to the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,27 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
+import { Poppins } from 'next/font/google'
 
 import '@/styles/globals.css'
+
+const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <>
+        <style jsx global>{`
+          html {
+            font-family: ${poppins.style.fontFamily};
+          }
+        `}</style>
+        <Component {...pageProps} />
+      </>
     </QueryClientProvider>
   )
 }
