f239f85e45871c1713e0a6bd55cc5d53775bca81
jv-vogler <jvsvogler@gmail.com>
2023-05-17 15:26:47 -0300

    feat: :art: make the logo a fixed header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,14 @@ const Home = () => {
   const { languages, technologies } = useRepos(data?.user && data?.user.repositories.edges)
 
   return (
-    <div className="flex flex-col gap-8 p-4 text-zinc-300">
-      <h1 className="select-none p-2 text-3xl font-bold">
-        <span className="border-b-4 border-orange-400">FetchHire</span>
-      </h1>
-      <div className="flex flex-col items-center gap-8">
+    <div className="flex flex-col gap-8 text-zinc-300">
+      <div className="fixed z-50 h-20 w-full bg-gradient-to-b from-zinc-900 via-[#232526] via-50% to-transparent">
+        <h1 className="select-none p-2 text-3xl font-bold">
+          <span className="border-b-4 border-orange-400">FetchHire</span>
+        </h1>
+      </div>
+
+      <div className="mt-16 flex flex-col items-center gap-8 p-4">
         <Searchbar setGithubUser={setGithubUser} status={status} />
         {data?.user && (
           <UserData data={data} languages={languages} technologies={technologies} />
