125ac666cbf95ae2df59b36f8a8b97a1724d5791
jv-vogler <jvsvogler@gmail.com>
2023-05-22 16:27:52 -0300

    feat: :sparkles: implement Ni18n internationalization

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
       },
     ],
   },
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'pt-BR'],
+  },
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { AppProps } from 'next/app'
 import { Poppins } from 'next/font/google'
+import { appWithI18Next } from 'ni18n'
 
 import '@/styles/globals.css'
+
+import { ni18nConfig } from '../../ni18n.config'
 
 const poppins = Poppins({
   weight: '400',
@@ -11,7 +14,7 @@ const poppins = Poppins({
 
 const queryClient = new QueryClient()
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <>
@@ -25,3 +28,5 @@ export default function App({ Component, pageProps }: AppProps) {
     </QueryClientProvider>
   )
 }
+
+export default appWithI18Next(App, ni18nConfig)
