20609d3f41493c7562d607ae1d532ae95e3e2188
jv-vogler <jvsvogler@gmail.com>
2023-05-24 16:59:05 -0300

    feat: :globe_with_meridians: add language toggle button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
+import { CircleFlag } from 'react-circle-flags'
+import { useTranslation } from 'react-i18next'
 import { MagnifyingGlass } from 'react-loader-spinner'
+import Switch from 'react-switch'
 
 import Footer from '@/components/Footer'
 import Searchbar from '@/components/Searchbar'
@@ -9,22 +12,49 @@ import { useRepos } from '@/hooks/useRepos'
 
 const Home = () => {
   const [githubUser, setGithubUser] = useState('')
-  const { data, error, status } = useQueryUserData(githubUser)
+  const { data, status } = useQueryUserData(githubUser)
   const { languages, technologies } = useRepos(data?.user && data?.user.repositories.edges)
+  const { t, i18n } = useTranslation()
+
+  function toggleLanguage() {
+    i18n.language === 'en' ? i18n.changeLanguage('pt-BR') : i18n.changeLanguage('en')
+  }
 
   return (
     <div className="flex h-screen flex-col gap-8 text-zinc-300">
-      <div className="fixed z-50 h-20 w-full bg-gradient-to-b from-zinc-900 via-[#232526] via-50% to-transparent">
+      <div className="fixed z-50 flex h-20 w-full items-center justify-between bg-gradient-to-b from-zinc-900 via-[#232526] via-50% to-transparent px-4">
         <h1 className="select-none p-2 text-3xl font-bold">
           <span className="border-b-4 border-orange-400">FetchHire</span>
         </h1>
+        <Switch
+          onChange={toggleLanguage}
+          checked={i18n.language === 'en'}
+          checkedIcon={false}
+          uncheckedIcon={false}
+          checkedHandleIcon={<CircleFlag countryCode="us" width={30} />}
+          uncheckedHandleIcon={<CircleFlag countryCode="br" width={30} />}
+          offColor="#555"
+          onColor="#555"
+          width={60}
+          height={30}
+          aria-label={t('ToggleLanguage') || 'Toggle language'}
+          tabIndex={0}
+        />
       </div>
 
       <div className="mt-16 flex flex-col items-center gap-8 p-4">
         <Searchbar setGithubUser={setGithubUser} status={status} />
         {status === 'error' && (
           <div className="-translate-y-7 text-sm text-red-500">
-            User <span className="font-bold">{githubUser}</span> not found.
+            {i18n.language === 'en' ? (
+              <>
+                User <span className="font-bold">{githubUser}</span> not found.
+              </>
+            ) : (
+              <>
+                Usuário <span className="font-bold">{githubUser}</span> não encontrado.
+              </>
+            )}
           </div>
         )}
 
@@ -49,4 +79,5 @@ const Home = () => {
     </div>
   )
 }
+
 export default Home
